508b6c03d6a986faa9711e25a460e01cf349b04c
Rufus <ruufman@gmail.com>
2019-01-28 21:41:35 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
+
 def square_array(array)
   # your code here
+  Array.new(array.size) { |i| array[i] ** 2}
+
 end
+end
+
+=begin
+def square_array(array)
+  new array [ ]
+  array.each { |i| new array << i ** 2}
+  new_array
+  =end
