318f29fd1723a6c447e6aa88ddd738fa63fa684d
Khang Le <khang.le27@gmail.com>
2019-02-14 14:38:14 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,23 +1,26 @@
-=begin
 def square_array(array)
   new_array=[]
   array.each do |element|
     new_array << element**2
   end 
-  puts new_array.inspect
+  #puts new_array.inspect
   new_array
 end
-=end
 
+#Using the collect method
+#def square_array(array)
+#  new_array = array.collect do |element|
+#    element**2
+#  end
+#  puts new_array.inspect
+#  new_array
+#end
 
-Using the collect method
-def square_array(array)
-  new_array = array.collect do |element|
-    element**2
-  end
-end
-
+#shorter using the collect method
+#def square_array(array)
+#  new_array = array.collect{|element| element**2}
+#end
 
 #running the method #square_array(array)
-test_array = [1,2,3]
-square_array(test_array)
+#test_array = [1,2,3]
+#square_array(test_array)
